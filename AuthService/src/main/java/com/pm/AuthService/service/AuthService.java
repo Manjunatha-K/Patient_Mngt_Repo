4d3540597a99c490769e48b7226fbe31a833eb69
Manjunatha <k.manjunatha1518@gmail.com>
2025-05-22 18:16:22 +0530

    Auth Service

---
+++ AuthService/src/main/java/com/pm/AuthService/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.pm.AuthService.service;
+
+import com.pm.AuthService.dto.LoginRequestDTO;
+import com.pm.AuthService.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword())).
+                map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
+        return token;
+    }
+
+}
